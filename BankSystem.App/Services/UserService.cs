4ef6cafae14b0594f5ce15abcd351062fd537ce6
temo14 <temo599922030@gmail.com>
2023-03-17 13:11:25 +0400

    add app project

---
+++ BankSystem.App/Services/UserService.cs
@@ -0,0 +1,39 @@
+﻿namespace BankSystem.App.Services;
+
+public class UserService
+{
+    private readonly HttpClient httpClient;
+
+    public UserService(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+
+    public async Task GetItem(int id)
+    {
+        //try
+        //{
+        //    var response = await httpClient.GetAsync($"api/Product/{id}");
+
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+        //        {
+        //            return default(ProductDto);
+        //        }
+
+        //        return await response.Content.ReadFromJsonAsync<ProductDto>();
+        //    }
+        //    else
+        //    {
+        //        var message = await response.Content.ReadAsStringAsync();
+        //        throw new Exception($"Http status code: {response.StatusCode} message: {message}");
+        //    }
+        //}
+        //catch (Exception)
+        //{
+        //    //Log exception
+        //    throw;
+        //}
+    }
+}
